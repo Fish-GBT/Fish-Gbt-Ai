3a540ba351c35aa5694f1eed16c929124504bc61
Fish <blobpie67@gmail.com>
2026-03-14 14:12:20 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -20,16 +20,20 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
+    if (!response.ok) {
+      return res.status(500).json({
+        reply: JSON.stringify(data)
+      });
+    }
+
     res.status(200).json({
       reply: data.choices[0].message.content
     });
 
   } catch (error) {
-
     res.status(500).json({
-      reply: "Server error"
+      reply: error.toString()
     });
-
   }
 
 }
